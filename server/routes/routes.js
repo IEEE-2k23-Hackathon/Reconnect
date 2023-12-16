b6fa09d83906fb7c1d091ae18556d90375f67b31
Dhanush B <talktodhanush4554@gmail.com>
2023-12-16 23:24:40 +0530

    get Everything Request updated

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const Register = require('../controllers/Register');
-const Login = require('../controllers/Login');
-const Logout = require('../controllers/Logout');
-const Verifytoken = require('../middlewares/verifyAuth');
+const express = require("express");
+const Register = require("../controllers/Register");
+const Login = require("../controllers/Login");
+const Logout = require("../controllers/Logout");
+const UserController = require("../controllers/UserController");
+const Verifytoken = require("../middlewares/verifyAuth");
 const route = express.Router();
 
 //Post Requests
-route.post('/register',Register);
-route.post('/login',Login);
-route.post('/logout',Verifytoken,Logout);
+route.post("/register", Register);
+route.post("/login", Login);
+route.post("/logout", Verifytoken, Logout);
 
 //Get Requests
-
+route.get("/getEverything", UserController.GetAllUserData);
 module.exports = route;
