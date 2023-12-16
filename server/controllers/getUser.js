75f0d3802f965cb2d531ce44e0a4c39e083ff600
Dhanush B <talktodhanush4554@gmail.com>
2023-12-16 23:55:06 +0530

    error handling

--- server/controllers/getUser.js
+++ server/controllers/getUser.js
@@ -1,20 +1,17 @@
 const User = require("../models/UserModel");
 
-const getUser = async (req,res)=>{
-    try {
-        const userID = req.id;
+const getUser = async (req, res) => {
+  try {
+    const userID = req.id;
 
-        const userData = await User.findById(userID,'-password');
+    const userData = await User.findById(userID, "-password");
 
-        userData ? res
-                .status(200)
-                .json({userData})
-             : res
-                .status(404)
-                .json({message:'User Not Found'});
-    } catch (error) {
-        
-    }
-}
+    userData
+      ? res.status(200).json({ userData })
+      : res.status(404).json({ message: "User Not Found" });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
-module.exports = {getUser};
+module.exports = { getUser };
